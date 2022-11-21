fc321b6f424ca48f27e6a647395465cf44cea6b7
Thomas DiGregorio <thomas.digregorio@gmail.com>
2022-11-21 12:54:26 -0500

    updated ui and added counter

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+
+
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     const random = Math.floor(Math.random() * choices.length);
@@ -5,9 +8,10 @@ function getComputerChoice() {
 
 }
 
-let computerSelection = getComputerChoice();
+let computerSelection = '';
 
 function playRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
     if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
         return "You win! Rock beats Scissors";
     } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
@@ -21,6 +25,54 @@ function playRound(playerSelection, computerSelection) {
     }   
 } 
 
-function game() {
 
-    
+const btnRock = document.querySelector('#one');
+const btnPaper = document.querySelector('#two');
+const btnScissor = document.querySelector('#three');
+
+let playerCount = 1;
+let computerCount = 1;
+
+btnRock.addEventListener('click', () => {
+    document.getElementById("results").innerHTML = (playRound("Rock", computerSelection));
+});
+
+btnRock.addEventListener('click', () => {
+    if (document.getElementById("results").innerHTML === "You win! Rock beats Scissors") {
+        document.getElementById("scores-player").innerHTML = playerCount++;
+    } else if (document.getElementById("results").innerHTML === "You Lose!") {
+               document.getElementById("scores-computer").innerHTML = computerCount++;
+    }
+});
+
+
+
+btnPaper.addEventListener('click', () => {
+    document.getElementById("results").innerHTML = (playRound("Paper", computerSelection));
+});
+
+btnPaper.addEventListener('click', () => {
+    if (document.getElementById("results").innerHTML === "You win! Paper covers Rock") {
+        document.getElementById("scores-player").innerHTML = playerCount++;
+    } else if (document.getElementById("results").innerHTML === "You Lose!") {
+               document.getElementById("scores-computer").innerHTML = computerCount++;
+    }
+});
+
+
+
+btnScissor.addEventListener('click', () => {
+    document.getElementById("results").innerHTML = (playRound("Scissors", computerSelection));
+});
+
+btnScissor.addEventListener('click', () => {
+    if (document.getElementById("results").innerHTML === "You win! Scissors beats paper") {
+        document.getElementById("scores-player").innerHTML = playerCount++;
+    } else if (document.getElementById("results").innerHTML === "You Lose!") {
+               document.getElementById("scores-computer").innerHTML = computerCount++;
+    }
+});
+
+
+
+
